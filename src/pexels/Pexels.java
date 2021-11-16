5e2a09c0c57202c94f78a32e173c6fd5b8a8f9b0
Alejandro Ramos <arhcoder@gmail.com>
2021-11-15 20:08:47 -0600

    Obtención y dibujado de imágen; implementado

--- src/pexels/Pexels.java
+++ src/pexels/Pexels.java
@@ -1,6 +1,7 @@
 package pexels;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
@@ -12,6 +13,7 @@ public class Pexels extends javax.swing.JFrame
 {
     // Attributes //
     File picture;
+    String path;
     BufferedImage ramBuffer;
     
     // Methods //
@@ -20,7 +22,7 @@ public class Pexels extends javax.swing.JFrame
         /// Accede al disco duro y obtiene un archivo que guarda en una secuencia de bytes.
         /// Regresa la secuencia de bits del archivo.
         
-        byte[] image = new byte[100 * 100];
+        byte[] image = new byte[999999];
         
         try
         {
@@ -62,21 +64,106 @@ public class Pexels extends javax.swing.JFrame
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Picture = new javax.swing.JLabel();
+        Menu = new javax.swing.JMenuBar();
+        Menu_File = new javax.swing.JMenu();
+        Menu_File_Open = new javax.swing.JMenuItem();
+        Menu_File_Save = new javax.swing.JMenuItem();
+        Menu_Operators = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Pexels");
+        setName("Pexels"); // NOI18N
+        setSize(new java.awt.Dimension(1024, 800));
+
+        Menu.setMinimumSize(new java.awt.Dimension(56, 100));
+
+        Menu_File.setText("Archivo");
+
+        Menu_File_Open.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_O, java.awt.event.InputEvent.CTRL_MASK));
+        Menu_File_Open.setText("Abrir");
+        Menu_File_Open.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Menu_File_OpenActionPerformed(evt);
+            }
+        });
+        Menu_File.add(Menu_File_Open);
+
+        Menu_File_Save.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.CTRL_MASK));
+        Menu_File_Save.setText("Guardar");
+        Menu_File_Save.setEnabled(false);
+        Menu_File_Save.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Menu_File_SaveActionPerformed(evt);
+            }
+        });
+        Menu_File.add(Menu_File_Save);
+
+        Menu.add(Menu_File);
+
+        Menu_Operators.setText("Operadores");
+        Menu.add(Menu_Operators);
+
+        setJMenuBar(Menu);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(Picture, javax.swing.GroupLayout.DEFAULT_SIZE, 624, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(Picture, javax.swing.GroupLayout.DEFAULT_SIZE, 479, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void Menu_File_OpenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Menu_File_OpenActionPerformed
+        
+        /// Abre un archivo.
+        
+        // Abre un selector de archivos.
+        JFileChooser fileChooser = new JFileChooser();
+        this.picture = fileChooser.getSelectedFile();
+        
+        // Filtra las extensiones de imagen.
+        if (picture.canRead())
+        {
+            if
+            (
+                picture.getName().endsWith("jpg") ||
+                picture.getName().endsWith("jpeg") ||
+                picture.getName().endsWith("png") ||
+                picture.getName().endsWith("bmp")
+            )
+            {
+                // Abre la imagen seleccionada.
+                byte[] image = openImage(picture);
+                Picture.setIcon(new ImageIcon(image));
+                
+                // Guarda la ruta de la imagen.
+                Toolkit tool = Toolkit.getDefaultToolkit();
+                this.path = fileChooser.getSelectedFile().toString();
+                
+                // Se ajusta la imágen al tamaño de la pestaña.
+                Image RAMImage = tool.createImage(path);
+                Picture.setIcon(new ImageIcon(RAMImage.getScaledInstance(Picture.getWidth(), Picture.getHeight(), Image.SCALE_AREA_AVERAGING)));
+
+                this.path = picture.getPath();
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(this, "¡No se pudo guardar!\nIntente de nuevo...\n",
+                "Algo salió mal", JOptionPane.ERROR);
+            }
+        }
+    }//GEN-LAST:event_Menu_File_OpenActionPerformed
+
+    private void Menu_File_SaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Menu_File_SaveActionPerformed
+        
+    }//GEN-LAST:event_Menu_File_SaveActionPerformed
 
     public static void main(String args[])
     {
@@ -110,5 +197,11 @@ public class Pexels extends javax.swing.JFrame
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuBar Menu;
+    private javax.swing.JMenu Menu_File;
+    private javax.swing.JMenuItem Menu_File_Open;
+    private javax.swing.JMenuItem Menu_File_Save;
+    private javax.swing.JMenu Menu_Operators;
+    private javax.swing.JLabel Picture;
     // End of variables declaration//GEN-END:variables
 }
